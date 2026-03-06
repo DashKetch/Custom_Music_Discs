c7b49068449af50d79e9479eda4dfe875d793963
Dash_Ketch <nateisthegreatgreat@gmail.com>
2026-03-05 18:25:14 -0800

    fix crashing on test

--- src/main/java/dashketch/mods/custom_music_discs/Custom_music_discs.java
+++ src/main/java/dashketch/mods/custom_music_discs/Custom_music_discs.java
@@ -19,11 +19,11 @@ public class Custom_music_discs {
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
 
     // Registering the two items
-    public static final DeferredItem<Item> BLANK_CUSTOM_DISC = ITEMS.registerSimpleItem("Blank Custom Disc", new Item.Properties().stacksTo(1));
-    public static final DeferredItem<Item> DISC_BURNER = ITEMS.registerSimpleItem("Disc Burner", new Item.Properties());
+    public static final DeferredItem<Item> BLANK_CUSTOM_DISC = ITEMS.registerSimpleItem("blank_custom_disc", new Item.Properties().stacksTo(1));
+    public static final DeferredItem<Item> DISC_BURNER = ITEMS.registerSimpleItem("disc_burner", new Item.Properties());
 
     // Registering the Creative Tab
-    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> CUSTOM_DISCS_TAB = CREATIVE_MODE_TABS.register("Custom Music Discs",
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> CUSTOM_DISCS_TAB = CREATIVE_MODE_TABS.register("custom_music_discs",
             () -> CreativeModeTab.builder()
                     .title(Component.translatable("itemGroup." + MODID))
                     .withTabsBefore(CreativeModeTabs.COMBAT)
