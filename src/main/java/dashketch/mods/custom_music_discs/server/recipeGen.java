647d965fdcc70051a5bdd3f50dfcbb6052519839
Dash_Ketch <nateisthegreatgreat@gmail.com>
2026-03-23 09:11:05 -0700

    fix recipeGen.java

--- src/main/java/dashketch/mods/custom_music_discs/server/recipeGen.java
+++ src/main/java/dashketch/mods/custom_music_discs/server/recipeGen.java
@@ -6,6 +6,7 @@ import net.minecraft.data.recipes.*;
 import net.minecraft.world.item.Items;
 import net.minecraft.resources.ResourceLocation;
 import org.jetbrains.annotations.NotNull;
+import dashketch.mods.custom_music_discs.item.ModItems;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -17,13 +18,13 @@ public class recipeGen extends RecipeProvider {
 
     @Override
     protected void buildRecipes(@NotNull RecipeOutput output) {
-        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, Items.MUSIC_DISC_5, 1)
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItems.BLANK_DISC, 1)
                 .requires(Items.DISC_FRAGMENT_5, 4)
                 .requires(Items.SLIME_BALL)
                 .unlockedBy("has_disc_fragment", has(Items.DISC_FRAGMENT_5))
                 .save(output, ResourceLocation.fromNamespaceAndPath("custom_music_discs", "blank_custom_disc"));
 
-        ShapedRecipeBuilder.shaped(RecipeCategory.DECORATIONS, Items.SPYGLASS, 1)
+        ShapedRecipeBuilder.shaped(RecipeCategory.TOOLS, ModItems.DISC_BURNER, 1)
                 .pattern("Z#Z")
                 .pattern("#X#")
                 .pattern("#Y#")
